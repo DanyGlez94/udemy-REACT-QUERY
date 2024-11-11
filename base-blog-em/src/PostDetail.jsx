77f7d55e896d0937a8b0dd5be53215191f22479c
Daniel Gonzalez <daniel_glez94@hotmail.com>
2024-11-11 05:32:56 -0600

    Query created for Blog comments

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,13 +1,30 @@
-import { fetchComments } from "./api";
-import "./PostDetail.css";
+import { fetchComments } from './api';
+import { useQuery } from '@tanstack/react-query';
+import './PostDetail.css';
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+
+  const { data, error, isError, isLoading } = useQuery({
+    queryKey: ['comments', post.id],
+    queryFn: () => fetchComments(post.id),
+  });
+
+  if (isLoading) {
+    return <h3>Loading comments...</h3>;
+  }
+
+  if (isError) {
+    return (
+      <>
+        <h3>Something went wrong...</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
 
   return (
     <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
+      <h3 style={{ color: 'blue' }}>{post.title}</h3>
       <button>Delete</button> <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
