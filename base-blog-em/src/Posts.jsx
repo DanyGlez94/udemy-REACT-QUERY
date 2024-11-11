c898a1dc133930900c694e2661495aa64641c95c
Daniel Gonzalez <daniel_glez94@hotmail.com>
2024-11-11 04:58:51 -0600

    Handling loading and error states

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -10,12 +10,18 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const { data } = useQuery({
+  const { data, isError, error, isLoading } = useQuery({
     queryKey: ['posts'],
     queryFn: fetchPosts,
   });
 
-  if(!data) { return <div>Loading...</div> }
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+
+  if (isError) {
+    return <><h3>Something went wrong...</h3><p>{error.toString()}</p></>;
+  }
 
   return (
     <>
