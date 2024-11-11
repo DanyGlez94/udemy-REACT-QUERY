f1bb9832a6253695d983a018eee73fe3baa6792d
Daniel Gonzalez <daniel_glez94@hotmail.com>
2024-11-11 04:49:03 -0600

    Added a Query Client and Provider, also created a query with useQuery

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 
-import { fetchPosts, deletePost, updatePost } from "./api";
-import { PostDetail } from "./PostDetail";
+import { fetchPosts, deletePost, updatePost } from './api';
+import { PostDetail } from './PostDetail';
 const maxPostPage = 10;
 
 export function Posts() {
@@ -9,7 +10,12 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const data = [];
+  const { data } = useQuery({
+    queryKey: ['posts'],
+    queryFn: fetchPosts,
+  });
+
+  if(!data) { return <div>Loading...</div> }
 
   return (
     <>
